6bb14db5be87aa6f9bb774ef48b3f3286f1d2125
Assisting <erik@eriklabine.com>
2020-12-15 18:28:49 -0600

    Rewrite functions to use sqlite for data storage

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const RssFeedEmitter = require('rss-feed-emitter');
 const feeder = new RssFeedEmitter({ skipFirstLoad: true });
 const discord = require('discord.js');
-const sqlite3 = require('sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 const { google } = require('googleapis'); 
 const { prefix } = require('./config.json');
 const { discordToken, youTubeToken } = require('./secrets.json');
@@ -9,56 +9,52 @@ const { discordToken, youTubeToken } = require('./secrets.json');
 const youTubeRSS = 'https://www.youtube.com/feeds/videos.xml?channel_id=';
 const discordClient = new discord.Client();
 const youTubeClient = google.youtube('v3');
-const database = new sqlite3.Database('./server-data.db');
-const serverChannel = {};
-const serverSubscription = {};
+const database = new sqlite3.Database('./server-data.db', console.log);
 
 discordClient.once('ready', () => startup());
 discordClient.on('message', (message) => messageHandler(message));
 discordClient.login(discordToken);
 feeder.on('new-item', function(item) {
-    alertTheMasses(item);
+    retrieveSubscribingGuilds(item);
 });
 feeder.on('error', console.error);
  
 const addChannel = (channelId) => {
     feeder.add({
         url: `${youTubeRSS}${channelId}`,
-        refresh: 600,
+        refresh: 60,
     });
 };
 
-const alertTheMasses = (item) => {
-    for (const server in serverSubscription)
-    {
-        if(serverSubscription[server].includes(item['yt:channelid']['#']))
-        {
-            textChannelFromChannelID(serverChannel[server]).send(item.link);
-        }
-    }
-};
-
 const defineBotRoom = (serverId, channel, silent) => {
-    serverChannel[serverId] = channel.channelId;
+    setGuildBotChannel(serverId, channel.id);
     if (!silent)
     {
         channel.send(`YouTube Feed will post to #${channel.name}`);
     }
 };
 
-const startup = () => {
-    console.log("loaded");
-    // Load the things from DB
+const startup = async () => {
+    database.run("CREATE TABLE GuildBotChannel (GuildId TEXT, BotChannelId TEXT)", [], (err) => {});
+    database.run("CREATE TABLE GuildYouTubeChannel (GuildId TEXT, YouTubeChannelId TEXT)", [], (err) => {});
+    refreshAllRSSFeeds();
+    discordClient.user.setActivity(prefix + 'ytf-help'); 
+    console.log('connected');
 };
 
 const messageHandler = (message) => {
-    if (message.content.toLowerCase().startsWith(prefix + 'youtubefeed')) {
-        console.log("add discord channel");
-        defineBotRoom(message.guild.id, message.channel, false);
-        serverSubscription[message.guild.id] = [];
-	} else if (message.content.toLowerCase().startsWith(prefix + 'youtubeadd')) {
-        console.log("add YT channel");
-        serverSubscribe(message, false);
+    if(message.member.hasPermission('ADMINISTRATOR')) {
+        if (message.content.toLowerCase().startsWith(prefix + 'youtubefeed')) {
+            console.log('add discord channel');
+            defineBotRoom(message.guild.id, message.channel, false);
+        } else if (message.content.toLowerCase().startsWith(prefix + 'youtubeadd')) {
+            console.log('add YT channel');
+            serverSubscribe(message, false);
+        } else if (message.content.toLowerCase().startsWith(prefix + 'ytf-help')) {
+            sendHelp(message.channel);
+        } else if (message.content.toLowerCase().startsWith(prefix + 'youtuberemove')) {
+            removeSubscription(message);
+        }
     }
 };
 
@@ -77,10 +73,10 @@ const serverSubscribe = (message, silent) => {
         } 
     } else if (message.content.split('youtubeadd')[1].trim().startsWith('UC')) {
         const channelId = message.content.split('youtubeadd')[1].trim();
-        serverSubscription[message.guild.id].push(channelId);
         addChannel(channelId);
+        addSubscription(message.guild.id, channelId)
     } else {
-        message.channel.send("Please either provide a channel ID (not a channel name) or a video link.");
+        message.channel.send('Please either provide a channel ID (not a channel name) or a video link.');
     }
 };
 
@@ -89,7 +85,7 @@ const subscribeFromVideoLink = async (videoId, message, silent) => {
         const videoResult = await youTubeClient.videos.list({key: youTubeToken, part: 'snippet', id: videoId});
         const videoData = videoResult.data.items[0].snippet;
         addChannel(videoData.channelId);
-        serverSubscription[message.guild.id].push(videoData.channelId);
+        addSubscription(message.guild.id, videoData.channelId)
         if (videoData.channelTitle && videoData.channelTitle.length > 0 && !silent)
         {
             message.channel.send(`Subscribing to ${videoData.channelTitle}`);
@@ -100,12 +96,108 @@ const subscribeFromVideoLink = async (videoId, message, silent) => {
 };
 
 const textChannelFromChannelID = (channelId) => {
-    for (const discordChannel in discordClient.channels.cache)
-    {
-        if (discordChannel.channelId == channelId) {
-            return discordChannel;
-        }
-    }
+    return discordClient.channels.cache.get(channelId);
+};
 
-    return null;
+const setGuildBotChannel = (guildId, channelId) => {
+    database.get('SELECT BotChannelId FROM GuildBotChannel WHERE GuildId = ?', [guildId], (err, row) => {
+        if (err) {
+            console.error(err);
+        }
+        
+        if (!row) {
+            createGuildBotChannel(guildId, channelId);
+        } else {
+            updateGuildBotChannel(guildId, channelId);
+        }
+    });
+};
+
+const createGuildBotChannel = (guildId, channelId) => {
+    database.run('INSERT INTO GuildBotChannel (GuildId, BotChannelId) VALUES (?, ?)', [guildId, channelId], (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+
+const updateGuildBotChannel = (guildId, channelId) => {
+    database.run('UPDATE GuildBotChannel SET BotChannelId = ? WHERE GuildId = ?', [channelId, guildId], (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+
+const postLinkInBotChannel = (guildId, link) => {
+    database.get('SELECT BotChannelId FROM GuildBotChannel WHERE GuildId = ?', [guildId], (err, row) => {
+        if (err) {
+            console.error(err);
+        }
+        textChannelFromChannelID(row.BotChannelId).send(link);
+    });
+};
+
+const retrieveSubscribingGuilds = (item) => {
+    const youTubeChannelId = item['yt:channelid']['#'];
+    database.each('SELECT GuildId FROM GuildYouTubeChannel WHERE YouTubeChannelId = ?', [youTubeChannelId], (err, row) => {
+        if (err) {
+            console.error(err);
+        }
+        postLinkInBotChannel(row.GuildId, item.link);
+    });
+};
+
+const addSubscription = (guildId, youTubeChannelId) => {
+    database.get('SELECT GuildId FROM GuildYouTubeChannel WHERE YouTubeChannelId = ?', [youTubeChannelId], (err, row) => {
+        if (err) {
+            console.error(err);
+        }
+        
+        if (!row) {
+            insertGuildYouTubeChannel(guildId, youTubeChannelId);
+        } else {
+            console.log('Duplicate subscription');
+        }
+    });
+};
+
+const insertGuildYouTubeChannel = (guildId, youTubeChannelId) => {
+    database.run('INSERT INTO GuildYouTubeChannel (GuildId, YouTubeChannelId) VALUES (?, ?)', [guildId, youTubeChannelId], (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+
+const refreshAllRSSFeeds = () => {
+    database.each('SELECT DISTINCT YouTubeChannelId FROM GuildYouTubeChannel', [], (err, row) => {
+        if (err) {
+            console.error(err);
+        }
+        addChannel(row.YouTubeChannelId);
+    });
+};
+
+const removeSubscription = (message) => {
+    const channelId = message.content.split('youtuberemove')[1].trim();
+    deleteGuildYouTubeChannel(message.guild.id, channelId);
+};
+
+const deleteGuildYouTubeChannel = (guildId, youTubeChannelId) => {
+    database.run('DELETE FROM GuildYouTubeChannel WHERE GuildId = ? AND YouTubeChannelId = ?', [guildId, youTubeChannelId], (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+
+const sendHelp = (channel) => {
+    channel.send(
+        'Help Menu:\n' +
+        `${prefix}youtubefeed - Sets the current channel as the destination for new YouTube videos in the feed.\n` +
+        `${prefix}youtubeadd <video link> - Adds the channel that published the video to the feed.\n` +
+        `${prefix}youtubeadd <channel ID> - Adds the specified channel (eg. UCK8sQmJBp8GCxrOtXWBpyEA) to the feed.\n` +
+        `${prefix}youtuberemove <channel ID> - Removes the specified channel (eg. UCK8sQmJBp8GCxrOtXWBpyEA) from the feed.`
+    );
 };
